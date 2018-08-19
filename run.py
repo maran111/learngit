0ff1c4ba38bcff7909f9e3e48cee8f23ada4f966
upupup <15620617204@163.com>
2018-08-19 15:44:32 +0800

    wrote a run.py file

---
+++ run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+#import sys
+from operator import and_
+
+from flask import request, jsonify, g, current_app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+import time
+# from apii.constant.Constant import Constant
+from app import create_app
+
+import flask, os,sys,time
+from flask import request, send_from_directory
+from flask import make_response
+
+
+
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    #print error
+    return jsonify({'status':'404', 'msg': 'page not found!'})
+
+if __name__ == "__main__":
+    # ConnectDB().monitor_to_db()
+    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=True)
+
+
+
+
+
